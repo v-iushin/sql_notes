3caa3a6d897474ba4ac619f5f20c792cf6c9bb69
v-iushin <yvo.yushin@gmail.com>
2026-08-09 23:56:41 +0300

    SQL: pgexercises, basic, extended

--- pgexercises/1_basic.sql
+++ pgexercises/1_basic.sql
@@ -30,3 +30,74 @@ SELECT
 	monthlymaintenance
 FROM cd.facilities
 WHERE membercost > 0 AND membercost < monthlymaintenance / 50.0;
+
+-- 5. produce a list of all facilities 
+-- with the word 'Tennis' in their name
+SELECT *
+FROM cd.facilities
+WHERE name LIKE '%Tennis%';
+
+-- 6. retrieve the details of facilities
+-- with ID 1 and 5,
+-- do it without using the OR operator
+SELECT *
+FROM cd.facilities
+WHERE facid IN (1, 5);
+
+-- 7. produce a list of facilities, 
+-- with each labelled as 'cheap' or 'expensive' 
+-- depending on if their monthly maintenance cost
+-- is more than $100. 
+-- return the name and monthly maintenance 
+-- of the facilities in question.
+SELECT
+	name,
+	CASE WHEN (monthlymaintenance > 100) THEN 'expensive'
+	ELSE 'cheap'
+	END AS cost
+FROM cd.facilities;
+
+-- 8. produce a list of members who joined after 
+-- the start of September 2012. 
+-- return the memid, surname, firstname, and 
+-- joindate of the members in question
+SELECT
+	memid,
+	surname,
+	firstname,
+	joindate
+FROM cd.members
+WHERE joindate >= '2012-09-01';
+
+-- 9. produce an ordered list of the first 10 surnames
+-- in the members table. 
+-- the list must not contain duplicates.
+SELECT DISTINCT surname
+FROM cd.members
+ORDER BY surname
+LIMIT 10;
+
+-- 10. a combined list of all surnames and 
+-- all facility names
+SELECT surname
+FROM cd.members
+	UNION
+SELECT name
+FROM cd.facilities;
+
+-- 11. get the signup date of your last member
+SELECT MAX(joindate) AS latest
+FROM cd.members;
+
+-- 12. get the first and last name 
+-- of the last member(s) who signed up
+SELECT 
+	firstname,
+	surname,
+	joindate
+FROM cd.members
+WHERE joindate = 
+	(
+		SELECT MAX(joindate)
+		FROM cd.members
+	);
