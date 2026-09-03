fc02a8b6b12af62e5a1a25fa08e9faaf85bec284
v-iushin <yvo.yushin@gmail.com>
2026-09-04 02:44:11 +0300

    SQL: pgexercises, aggregation, extended

--- pgexercises/4_aggregation.sql
+++ pgexercises/4_aggregation.sql
@@ -181,20 +181,219 @@ FROM cd.bookings AS b
 GROUP BY f.facid, f.name
 ORDER BY f.facid;
 
--- 14. 
+-- 14. produce a list of each member name, id,
+-- and their first booking after 
+-- September 1st 2012
+SELECT
+	m.surname,
+	m.firstname,
+	m.memid,
+	MIN(b.starttime) AS starttime
+FROM cd.members AS m
+	INNER JOIN cd.bookings AS b
+	ON m.memid = b.memid
+WHERE b.starttime >= '2012-09-01'
+GROUP BY m.surname, m.firstname, m.memid
+ORDER BY m.memid;
 
--- 15. 
+-- 15. produce a list of member names,
+-- with each row containing the 
+-- total member count.
+-- order by join date, and include guest members
+/*
+SELECT
+	COUNT(*) OVER(),
+	firstname,
+	surname
+FROM cd.members
+ORDER BY joindate;
+*/
+SELECT
+	(
+	    SELECT COUNT(*) FROM cd.members
+	) AS count,
+	firstname,
+	surname
+FROM cd.members
+ORDER BY joindate;
 
--- 16. 
+-- 16. produce a monotonically increasing
+-- numbered list of members (including guests),
+-- ordered by their date of joining
+SELECT
+	ROW_NUMBER() OVER(ORDER BY joindate),
+	firstname,
+	surname
+FROM cd.members
+ORDER BY joindate;
 
--- 17. 
+-- 17. output the facility id that has
+-- the highest number of slots booked.
+-- ensure that in the event of a tie,
+-- all tieing results get output
+SELECT
+	facid,
+	total
+FROM
+	(
+	    SELECT
+	  		facid,
+	  		SUM(slots) AS total,
+	  		RANK() OVER(ORDER BY SUM(slots) DESC) AS rank
+	  	FROM cd.bookings
+	  	GROUP BY facid
+	) AS subq
+WHERE rank = 1;
 
--- 18. 
+-- 18. produce a list of members (including guests),
+-- along with the number of hours they've booked
+-- in facilities, rounded to the nearest ten hours.
+-- rank them by this rounded figure,
+-- producing output of first name, surname,
+-- rounded hours, rank. 
+-- sort by rank, surname, and first name
+SELECT
+	m.firstname,
+	m.surname,
+	ROUND((SUM(b.slots) * 0.5), -1) AS hours,
+	RANK() OVER(ORDER BY ROUND((SUM(b.slots) * 0.5), -1) DESC) AS rank
+FROM cd.members AS m
+	INNER JOIN cd.bookings AS b
+	ON m.memid = b.memid
+GROUP BY m.memid
+ORDER BY rank, m.surname, m.firstname;
 
--- 19. 
+-- 19. produce a list of the top three revenue
+-- generating facilities (including ties)
+SELECT
+	name,
+	rank
+FROM
+	(
+	    SELECT
+	  		f.name AS name,
+	  		RANK() OVER(ORDER BY 
+							SUM(CASE
+			  					WHEN b.memid = 0 THEN b.slots * f.guestcost
+								ELSE b.slots * f.membercost
+							END)
+					   DESC) AS rank
+		FROM cd.bookings AS b
+			INNER JOIN cd.facilities AS f
+			ON b.facid = f.facid
+		GROUP BY f.name
+	) AS subq
+WHERE rank <= 3
+ORDER BY rank;
 
--- 20. 
+-- 20. classify facilities into equally sized groups
+-- of high, average, and low based on their revenue
+/* 
+WITH 
+reven AS (
+	SELECT
+  		f.name AS name,
+  		SUM(CASE
+			WHEN memid = 0 THEN b.slots * f.guestcost
+			ELSE b.slots * f.membercost
+		END) AS rev
+    FROM cd.bookings AS b
+		INNER JOIN cd.facilities AS f
+		ON b.facid = f.facid
+	GROUP BY f.name
+),
+max_rev AS (
+	SELECT MAX(rev) AS max FROM reven
+),
+min_rev AS (
+	SELECT MIN(rev) AS min FROM reven
+),
+range_rev AS (
+	SELECT (max - min) AS range FROM max_rev, min_rev
+),
+grp AS (
+	SELECT 
+		min + (range / 3.0) AS bot, 
+		min + (range * 2.0 / 3.0) AS top
+	FROM range_rev, min_rev
+)
+SELECT
+	name,
+	rev,
+	CASE
+		WHEN rev >= top THEN 'high'
+		WHEN rev >= bot THEN 'average'
+		ELSE 'low'
+	END AS revenue,
+	bot,
+	top
+FROM reven, grp
+ORDER BY rev DESC;
+*/
+SELECT
+	name,
+	CASE
+		WHEN class = 1 THEN 'high'
+		WHEN class = 2 THEN 'average'
+		ELSE 'low'
+	END AS revenue
+FROM 
+	(
+		SELECT
+			f.name AS name,
+			NTILE(3) OVER(ORDER BY 
+							SUM(CASE
+								WHEN b.memid = 0 THEN b.slots * f.guestcost
+								ELSE b.slots * f.membercost
+							END) DESC
+						) AS class
+		FROM cd.bookings AS b
+			INNER JOIN cd.facilities AS f
+			ON b.facid = f.facid
+		GROUP BY f.name
+	) AS subq
+ORDER BY class, name;
 
--- 21. 
+-- 21. based on the 3 complete months of data
+-- so far, calculate the amount of time
+-- each facility will take to repay its cost
+-- of ownership. 
+-- remember to take into account ongoing monthly
+-- maintenance. 
+-- output facility name and payback time
+-- in months, order by facility name
+SELECT
+	f.name AS name,
+	f.initialoutlay / ((SUM(CASE
+							WHEN b.memid = 0 THEN b.slots * f.guestcost
+							ELSE b.slots * f.membercost
+						END) / 3) - f.monthlymaintenance) AS months
+FROM cd.bookings AS b
+	INNER JOIN cd.facilities AS f
+	ON b.facid = f.facid
+GROUP BY f.facid
+ORDER BY name;
 
--- 22. 
+-- 22. for each day in August 2012, calculate
+-- a rolling average of total revenue over
+-- the previous 15 days.
+-- output should contain date and revenue
+-- columns, sorted by the date
+SELECT
+	dategen.date,
+	(
+		SELECT SUM(CASE
+					WHEN b.memid = 0 THEN b.slots * f.guestcost
+					ELSE b.slots * f.membercost
+				END) AS rev
+		FROM cd.bookings AS b
+			INNER JOIN cd.facilities AS f
+			ON b.facid = f.facid
+		WHERE b.starttime > dategen.date - INTERVAL '14 day'
+			AND b.starttime < dategen.date + INTERVAL '1 day'
+	) / 15 AS revenue
+FROM
+	(
+		SELECT GENERATE_SERIES(TIMESTAMP '2012-08-01', '2012-08-31', '1 day')::DATE AS date
+	) AS dategen
+ORDER BY dategen.date;
